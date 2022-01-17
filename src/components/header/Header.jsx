681be4b5871d9ba3615677416d7a2d1364ef2311
PhanVanThanh-hub <thanhphan1230@gmail.com>
2022-01-17 19:44:24 +0700

    Rework header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,28 @@
-import React  from 'react';
+import React ,{useLayoutEffect,useState} from 'react';
 import { makeStyles } from '@mui/styles';
 import { AppBar, Toolbar } from '@mui/material';
 import { Link } from 'react-router-dom';
 import Model from '../../layout/components/layout-model/layoutModel';
 import DrawerUI from './Drawer';
- 
+import image from '../../static/image/header/Pusheen_Zoom_Background_Campfire_2020.jpg';
 import {
     
    useMediaQuery,
  } from "@mui/material";
 const useStyles = makeStyles((theme) => ({
-  
+   root: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100%',
+      fontFamily: 'Nunito',
+      backgroundImage:`url(${image})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+    
+   },
    appbar: {
-      background: "transparent"
+      backgroundImage:`url(${image})`,
    },
    appbarWrapper: {
       width:"80%",
@@ -34,39 +44,90 @@ const useStyles = makeStyles((theme) => ({
          color:"rgba(19, 165, 229, 0.8)",
       },
       
-   }
-   
+   },
+   logo:{
+       
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      marginTop: "-50px",
+      marginLeft: "-50px",
+ 
+   },
+   container: {
+      textAlign: 'center',
+   },
+   title: {
+      color: '#fff',
+      fontSize: '4.5rem',
+   },
+   img:{
+      transition: "1s",
+      position: "absolute",
+      height:"100%",
+      width:"100%",
+      backgroundImage:`url(${image})`,
+   },
 }));
 
 export default function Header() {
    const classes = useStyles();
+   const [sticky , setSticky]= useState(true)
    const largeScreen = useMediaQuery('(min-width:480px)');
+   useLayoutEffect(() => {
+      function updatePosition() {
+         if(window.pageYOffset>0){
+            setSticky(false)
+         }
+         else{
+            setSticky(true)
+         }
+      }
+      window.addEventListener('scroll', updatePosition);
+      updatePosition();
+   }, );
    return (
-      <AppBar className={classes.appbar}  elevation={1}sx={{background:"#fff"}}>
-         <Toolbar className={classes.appbarWrapper}>
-            <h1 className={classes.appbarTitle}>
-               My<span className={classes.colorText}>Profile.</span> 
-            </h1>
-            {
-               largeScreen?
-               <div style={{display: "flex",flexDirection: "row",marginLeft: "16px",webkitBoxAlign: "center",
-                           alignItems: "center",}}>
-                  <Link to="/" className={classes.link}  >
-                              Home
-                  </Link>
-            
-                  <Link to="/cv" className={classes.link}>
-                     CV
-                  </Link>
-                  <Model/>
+      <AppBar elevation={1}sx={{transition: "1s",background:"#fff",height: sticky ? "100vh":"10vh"}}>
+         {
+            sticky?
+               <div className={classes.root} id="header" >
+                  <div className={classes.container}>
+                     <h1 className={classes.title}>
+                        Welcome to <br />
+                        My<span className={classes.colorText}>Profile</span>
+                     </h1>  
+                    
+                  </div>
                </div>
-               :<>
-                  <DrawerUI/>
-                  <Model/>
-               </>
-            }
-             
-         </Toolbar>
+            :
+            <>
+               <Toolbar className={classes.appbarWrapper}>
+                  <h1 className={classes.appbarTitle}>
+                     My<span className={classes.colorText}>Profile.</span> 
+                  </h1>
+                  {
+                     largeScreen?
+                     <div style={{display: "flex",flexDirection: "row",marginLeft: "16px",webkitBoxAlign: "center",
+                                 alignItems: "center",}}>
+                        <Link to="/" className={classes.link}  >
+                                    Home
+                        </Link>
+                  
+                        <Link to="/cv" className={classes.link}>
+                           CV
+                        </Link>
+                        <Model/>
+                     </div>
+                     :<>
+                        <DrawerUI/>
+                        <Model/>
+                     </>
+                  }
+                  
+               </Toolbar>
+            </>
+         }
+          
       </AppBar>
    );
 }
--- src/feature/home/index.jsx
+++ src/feature/home/index.jsx
@@ -1,73 +1,11 @@
  
 import HomePage from './page/PageHome';
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@mui/styles';
-import { IconButton, Collapse } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { Link as Scroll } from 'react-scroll';
+import React from 'react';
 import Box from "@mui/material/Box";
-const useStyles = makeStyles((theme) => ({
-   root: {
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-      fontFamily: 'Nunito',
-      backgroundImage:`url(${process.env.PUBLIC_URL + '/assets/9387992c8d13663da62cf78b0e1b9d9e.jpg'}) `,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-    
-   },
-   container: {
-      textAlign: 'center',
-   },
-   icon: {
-      color: '#fff',
-      fontSize: '2rem',
-   },
-   colorText: {
-      color: '#5AFF3D',
-   },
- 
-   title: {
-      color: '#fff',
-      fontSize: '4.5rem',
-   },
-   goDown: {
-      color: '#5AFF3D',
-      fontSize: '4rem',
-   },
-   
-}));
+
 function Home() {
-   const classes = useStyles();
-   const [checked, setChecked] = useState(false);
- 
-   useEffect(() => {
-      setChecked(true);
-   }, []);
    return (
       <div  >
-         <div className={classes.root} id="header" >
-            <Collapse id ="collapse"
-            in={checked}
-            {...(checked ? { timeout: 3000 } : {})}
-            collapsedheight={50}
-            >
-            <div className={classes.container}>
-               <h1 className={classes.title}>
-                  Welcome to <br />
-                  My<span className={classes.colorText}>Profile</span>
-               </h1>
-               <Scroll to="information" smooth={true}>
-                  <IconButton>
-                  <ExpandMoreIcon className={classes.goDown} />
-                  </IconButton>
-               </Scroll>
-            </div>
-            </Collapse>
-         </div>
-   
          <Box  sx={{   bgcolor: 'background.primary', color: 'text.primary', }}>
             <HomePage/>
          
